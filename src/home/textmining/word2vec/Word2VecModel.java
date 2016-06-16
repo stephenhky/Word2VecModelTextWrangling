43e9d4448d0f632f79a17d7b1351bba53e1df0c8
stephenhky <stephenhky@yahoo.com.hk>
2016-06-16 12:04:54 -0400

    remove inappropriate iterator (wrong model)

--- src/home/textmining/word2vec/Word2VecModel.java
+++ src/home/textmining/word2vec/Word2VecModel.java
@@ -28,10 +28,6 @@ public class Word2VecModel {
 
     public Map<String, double[]> getModelHashMap() { return modelHashMap; }
 
-    public Iterator<String> getSOIterator() {
-        return modelHashMap.keySet().iterator();
-    }
-
     @Override
     public String toString() {
         StringBuffer sb = new StringBuffer();
